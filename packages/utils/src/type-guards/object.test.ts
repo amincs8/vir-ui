f46ed1a2c9362df53c32cfd6cdb8018590d91fbf
AminCS <amincs@outlook.com>
2026-07-28 01:08:29 +0330

    feat(utils): add object type guards
    functions:
     - isObject
     - isObjectOfType
     - isPlainObject

---
+++ packages/utils/src/type-guards/object.test.ts
@@ -0,0 +1,85 @@
+import { describe, expect, it } from "vitest";
+import { isObject, isObjectOfType, isPlainObject } from "./object";
+
+describe("Type Guards: Object", () => {
+  it.for([
+    {
+      value: {},
+      instanceOf: Object,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: true,
+      },
+    },
+    {
+      value: [],
+      instanceOf: ArrayBuffer,
+      expectations: {
+        isObject: true,
+        isObjectOfType: false,
+        isPlainObject: false,
+      },
+    },
+    {
+      value: [],
+      instanceOf: Array,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: false,
+      },
+    },
+    {
+      value: [1, 2, 3],
+      instanceOf: Object,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: false,
+      },
+    },
+    {
+      value: new Date(),
+      instanceOf: Date,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: false,
+      },
+    },
+    {
+      value: new Set(),
+      instanceOf: Set,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: false,
+      },
+    },
+    {
+      value: new Map(),
+      instanceOf: Map,
+      expectations: {
+        isObject: true,
+        isObjectOfType: true,
+        isPlainObject: false,
+      },
+    },
+
+    { value: "string", expected: false },
+    { value: "", expected: false },
+    { value: undefined, expected: false },
+    { value: 100, expected: false },
+    { value: false, expected: false },
+    { value: true, expected: false },
+    { value: () => 0, expected: false },
+  ])(
+    "array predicates for $value, $instanceOf",
+    ({ value, expectations, expected, instanceOf = Object }) => {
+      expect(isObject(value)).toBe(expected ?? expectations.isObject);
+      expect(isObjectOfType(value, instanceOf)).toBe(expected ?? expectations.isObjectOfType);
+      expect(isPlainObject(value)).toBe(expected ?? expectations.isPlainObject);
+    },
+  );
+});
---
+++ packages/utils/src/type-guards/object.ts
@@ -0,0 +1,22 @@
+export function isObject (value: unknown): value is object {
+  return typeof value === "object" && value !== null;
+}
+
+
+export function isObjectOfType<T extends object> (
+  value: unknown,
+  prototype: new (...args: any[]) => T,
+): value is T {
+  return (
+    isObject(value) &&
+    value instanceof prototype
+  );
+}
+
+export function isPlainObject (value: unknown): value is Record<string, unknown> {
+  return (
+    value !== null &&
+    typeof value === "object" &&
+    Object.getPrototypeOf(value) === Object.prototype
+  );
+}

b65e716c2786d2caa1474fec20f6cf62e2f278d3
AminCS <amincs@outlook.com>
2026-07-28 00:38:19 +0330

    feat(utils): add isNull type-guards functions

---
+++ packages/utils/src/type-guards/null.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { isNull } from "./null";
+
+describe("Type Guards: Null", () => {
+  it.for([
+    { value: null, expected: true },
+    { value: undefined, expected: false },
+    { value: "string", expected: false },
+    { value: 100, expected: false },
+    { value: false, expected: false },
+    { value: true, expected: false },
+    { value: [], expected: false },
+    { value: [1, 2, 3], expected: false },
+    { value: {}, expected: false },
+    { value: { foo: true }, expected: false },
+    { value: new Date(), expected: false },
+    { value: new Set(), expected: false },
+    { value: new Map(), expected: false },
+    { value: () => 0, expected: false },
+  ])("null predicates for $value", ({ value, expected }) => {
+    expect(isNull(value)).toBe(expected);
+  });
+});

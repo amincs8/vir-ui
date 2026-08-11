2843652c45541609f59ef2b288132eb45f87bf52
AminCS <amincs@outlook.com>
2026-08-11 17:06:04 +0330

    feat(utils): add type-guards/isNonNullable fucntion

--- packages/utils/src/type-guards/null.test.ts
+++ packages/utils/src/type-guards/null.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { isNull } from "./null";
+import { isNonNullable, isNull } from "./null";
 
 describe("Type Guards: Null", () => {
   it.for([
@@ -19,5 +19,26 @@ describe("Type Guards: Null", () => {
     { value: () => 0, expected: false },
   ])("null predicates for $value", ({ value, expected }) => {
     expect(isNull(value)).toBe(expected);
+  });
+});
+
+describe("Type Guards: isNonNullable", () => {
+  it.for([
+    { value: null, expected: false },
+    { value: undefined, expected: false },
+    { value: "string", expected: true },
+    { value: 100, expected: true },
+    { value: false, expected: true },
+    { value: true, expected: true },
+    { value: [], expected: true },
+    { value: [1, 2, 3], expected: true },
+    { value: {}, expected: true },
+    { value: { foo: true }, expected: true },
+    { value: new Date(), expected: true },
+    { value: new Set(), expected: true },
+    { value: new Map(), expected: true },
+    { value: () => 0, expected: true },
+  ])("isNonNullable predicates for $value", ({ value, expected }) => {
+    expect(isNonNullable(value)).toBe(expected);
   });
 });
--- packages/utils/src/type-guards/null.ts
+++ packages/utils/src/type-guards/null.ts
@@ -1,3 +1,7 @@
 export function isNull (value: unknown): value is null {
   return value === null;
 }
+
+export function isNonNullable <T> (value: T): value is NonNullable<T> {
+  return value !== null && value !== undefined;
+}

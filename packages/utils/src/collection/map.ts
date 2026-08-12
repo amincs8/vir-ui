8a022eeb2cc7ea9c01f48ba9074cb45488c3ac14
AminCS <amincs@outlook.com>
2026-08-12 15:11:29 +0330

    refactor(utils): update param names of collection/map function

--- packages/utils/src/collection/map.ts
+++ packages/utils/src/collection/map.ts
@@ -1,17 +1,17 @@
 import { isFunction, isIterable, isPlainObject } from "../type-guards";
 import { PlainObject } from "../type-guards/type-guards";
 
-export function map<T = unknown, R = unknown> (values: Iterable<T> | PlainObject<T>, callback: (value: T) => R): R[] {
+export function map<T = unknown, R = unknown> (values: Iterable<T> | PlainObject<T>, iteratee: (value: T) => R): R[] {
   const result: R[] = [];
 
-  if (isFunction(callback)) {
+  if (isFunction(iteratee)) {
     if (isIterable(values)) {
       for (const value of values) {
-        result.push(callback(value));
+        result.push(iteratee(value));
       }
     } else if (isPlainObject(values)) {
       for (const key in values) {
-        result.push(callback(values[key] as T));
+        result.push(iteratee(values[key] as T));
       }
     }
   }

4a4de2e41faab50e5f46a9320620f68fc366f435
AminCS <amincs@outlook.com>
2026-08-31 19:30:37 +0330

    refactor(styles): use generateColorVarLine in generateColors

--- packages/styles/src/tw/generator/colors.test.ts
+++ packages/styles/src/tw/generator/colors.test.ts
@@ -1,76 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { generateColors } from "./colors";
 import { Theme } from "@/types";
+import { EXPECTED, VIR_THEME } from "../test/consts-test";
 
 describe("Tailwind Generators: Colors", () => {
   it("should generate CSS variables for colors", () => {
-    const color = {
-      DEFAULT: ".white",
-      on: ".gray.200",
-      hover: ".gray.400",
-      active: ".gray.400",
-      container: ".gray.200",
-      containerHover: ".gray.200",
-      onContainer: "..info.800",
-    };
-    const colors = {
-      primary: "#000000",
-      secondary: {
-        50: "#f5f5f5",
-        100: "#e5e5e5",
-      },
-      blue: "blue",
-      non: undefined,
-      non_2: null,
-      tertiary: color,
-      success: color,
-      warning: color,
-      danger: color,
-      info: color,
-    };
-
-    const result = generateColors(colors as unknown as Theme["colors"], "base");
-    expect(result).toBe(
-      `--color-primary: #000000;
---color-secondary-50: #f5f5f5;
---color-secondary-100: #e5e5e5;
---color-blue: blue;
---color-tertiary: var(--color-white);
---color-tertiary-on: var(--color-gray-200);
---color-tertiary-hover: var(--color-gray-400);
---color-tertiary-active: var(--color-gray-400);
---color-tertiary-container: var(--color-gray-200);
---color-tertiary-container-hover: var(--color-gray-200);
---color-tertiary-on-container: var(--base-info-800);
---color-success: var(--color-white);
---color-success-on: var(--color-gray-200);
---color-success-hover: var(--color-gray-400);
---color-success-active: var(--color-gray-400);
---color-success-container: var(--color-gray-200);
---color-success-container-hover: var(--color-gray-200);
---color-success-on-container: var(--base-info-800);
---color-warning: var(--color-white);
---color-warning-on: var(--color-gray-200);
---color-warning-hover: var(--color-gray-400);
---color-warning-active: var(--color-gray-400);
---color-warning-container: var(--color-gray-200);
---color-warning-container-hover: var(--color-gray-200);
---color-warning-on-container: var(--base-info-800);
---color-danger: var(--color-white);
---color-danger-on: var(--color-gray-200);
---color-danger-hover: var(--color-gray-400);
---color-danger-active: var(--color-gray-400);
---color-danger-container: var(--color-gray-200);
---color-danger-container-hover: var(--color-gray-200);
---color-danger-on-container: var(--base-info-800);
---color-info: var(--color-white);
---color-info-on: var(--color-gray-200);
---color-info-hover: var(--color-gray-400);
---color-info-active: var(--color-gray-400);
---color-info-container: var(--color-gray-200);
---color-info-container-hover: var(--color-gray-200);
---color-info-on-container: var(--base-info-800);
-`,
+    const result = generateColors(
+      {
+        ...VIR_THEME.colors,
+        ...VIR_THEME.semanticColors,
+      } as unknown as Theme["colors"],
+      VIR_THEME.prefix,
     );
+
+    expect(result.trim()).toBe([EXPECTED.colors, EXPECTED.semanticColors].join("\n"));
   });
 });
--- packages/styles/src/tw/generator/colors.ts
+++ packages/styles/src/tw/generator/colors.ts
@@ -1,33 +1,26 @@
-import { isPlainObject, isStringNonEmpty, kebabCase } from "@vir/utils";
+import { isPlainObject } from "@vir/utils";
 import { Theme } from "@/types";
-import { COLOR_VARIABLE_PREFIX, DEFAULT_ROLE_NAME } from "./consts";
-import { generateTailwindThemeValue } from "@/private-utils";
+import { DEFAULT_ROLE_NAME } from "./consts";
+import { generateColorVarLine } from "../utils/color";
 
 export function generateColors (colors: Theme["colors"], themePrefix: string): string {
   let result = "";
 
   if (isPlainObject(colors)) {
     for (const colorName in colors) {
-      const colorValue = colors[colorName];
-      if (isPlainObject(colorValue)) {
-        for (const scale in colorValue) {
-          const value = generateTailwindThemeValue({
-            value: colorValue[scale]!,
-            type: "color",
-            themePrefix,
-          });
-          if (scale === DEFAULT_ROLE_NAME) {
-            result += `${COLOR_VARIABLE_PREFIX}${colorName}: ${value};\n`;
-          } else {
-            result += `${COLOR_VARIABLE_PREFIX}${colorName}-${kebabCase(scale)}: ${value};\n`;
-          }
-        }
-      } else if (isStringNonEmpty(colorValue)) {
-        result += `${COLOR_VARIABLE_PREFIX}${colorName}: ${generateTailwindThemeValue({
-          value: colorValue!,
-          type: "color",
+      const colorValue = isPlainObject(colors[colorName])
+        ? colors[colorName]
+        : {
+          [DEFAULT_ROLE_NAME]: colors[colorName]!,
+        };
+
+      for (const scale in colorValue) {
+        result = `${result}${generateColorVarLine({
+          colorValue: colorValue[scale]!,
+          colorScale: scale,
+          colorName: colorName,
           themePrefix,
-        })};\n`;
+        })}\n`;
       }
     }
   }

d6117a954808eca69fc66e713698c9d63f0fc8f1
AminCS <amincs@outlook.com>
2026-09-01 14:26:27 +0330

    feat(styles): add tw/utils/generateVarLine function

--- packages/styles/src/tw/utils/var.test.ts
+++ packages/styles/src/tw/utils/var.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { generateVarName } from "./var";
+import { generateVarLine, generateVarName } from "./var";
 
 describe("Tailwind/utils: generateVarName", () => {
   it.for([
@@ -9,5 +9,39 @@ describe("Tailwind/utils: generateVarName", () => {
   ])("Should return $expected for $values args", ({ values, expected }) => {
     expect(generateVarName(values)).toBe(expected);
     expect(generateVarName(...values)).toBe(expected);
+  });
+});
+
+describe("Tailwind/utils: generateVarLine", () => {
+  it.for([
+    {
+      values: {
+        type: "color",
+        name: "primary",
+        value: ".blue.500",
+        themePrefix: "base",
+      },
+      expected: "--color-primary: var(--color-blue-500);",
+    },
+    {
+      values: {
+        type: "color",
+        name: "secondary",
+        value: ".red.500",
+        themePrefix: "base",
+      },
+      expected: "--color-secondary: var(--color-red-500);",
+    },
+    {
+      values: {
+        type: "color",
+        name: ["tertiary", "on", "container"],
+        value: ".green.500",
+        themePrefix: "base",
+      },
+      expected: "--color-tertiary-on-container: var(--color-green-500);",
+    },
+  ])("Should return $expected for $values args", ({ values, expected }) => {
+    expect(generateVarLine(values as Parameters<typeof generateVarLine>[0])).toBe(expected);
   });
 });
--- packages/styles/src/tw/utils/var.ts
+++ packages/styles/src/tw/utils/var.ts
@@ -1,7 +1,15 @@
-import { isArray, isStringNonEmpty, kebabCase } from "@vir/utils";
-import { VAR_PREFIX, VAR_SEPARATOR } from "./consts";
+import { isArray, isStringNonEmpty, kebabCase, Numberish } from "@vir/utils";
+import { DEFAULT_ROLE_NAME, TypePrefix, VAR_PREFIX, VAR_SEPARATOR } from "./consts";
+import { generateTailwindThemeValue } from "@/private-utils";
 
 type Arg = string | undefined;
+
+interface GenerateVarLineOptions {
+  type: keyof typeof TypePrefix;
+  name: string | string[];
+  value: Numberish;
+  themePrefix: string;
+}
 
 export function generateVarName(...values: Arg[]): string;
 export function generateVarName(values: Arg[]): string;
@@ -15,10 +23,25 @@ export function generateVarName (...values: [Arg[]] | Arg[]): string {
   }
 
   for (let i = 1; i < parts.length; ++i) {
-    if (isStringNonEmpty(parts[i])) {
+    if (isStringNonEmpty(parts[i]) && parts[i] !== DEFAULT_ROLE_NAME) {
       result = `${result}${VAR_SEPARATOR}${kebabCase(parts[i]! as string)}`;
     }
   }
 
   return result;
+}
+
+export function generateVarLine (options: GenerateVarLineOptions): string {
+  const generatedValue = generateTailwindThemeValue({
+    value: options.value,
+    type: options.type,
+    themePrefix: options.themePrefix,
+  });
+
+  const varName = generateVarName([
+    TypePrefix[options.type].prefix,
+    ...(isArray(options.name) ? options.name : [options.name]),
+  ]);
+
+  return `${varName}: ${generatedValue || "\"\""};`;
 }
